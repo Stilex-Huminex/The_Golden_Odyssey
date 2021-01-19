9c9a0bacbe2caa4dba0c6a253f46d45acefef941
Fabien Zoccola <biothewolff@gmx.fr>
2021-01-19 23:00:05 +0100

    feat(koldia): changing triggers

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private int id;
+
+    public void Interact()
+    {
+        GameObject go = this.gameObject;
+
+        // 1 - Lampadaires
+        if (id == 1)
+        {
+            ParticleSystem ps = go.GetComponent<ParticleSystem>();
+
+            if (!ps.main.playOnAwake) {
+                ps.Play();
+            }
+        }
+
+        // 2 - cailloux (grotte glace)
+        if (id == 2)
+        {
+            go.SetActive(false);
+        }
+    }
+}

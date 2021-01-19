11276655828b5abf388843d2ee1d19ee8c83f09f
1Samper1 <quentin.arzalier@free.fr>
2021-01-19 12:06:59 +0100

    Protoype radar terminus

--- Assets/Radar.cs
+++ Assets/Radar.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,6 +10,8 @@ public class Radar : MonoBehaviour
     List<GameObject> pointeurs;
     List<GameObject> pointBordure;
     [SerializeField] private GameObject point;
+    [SerializeField] private float distance;
+    [SerializeField] private Transform helper;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +22,34 @@ public class Radar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0;  i < pointeurs.Count; i++)
+        {
+            if (Vector3.Distance(pointeurs[i].transform.position, transform.position) > distance)
+            {
+                helper.LookAt(pointeurs[i].transform);
+                pointBordure[i].transform.position = transform.position + distance * helper.forward;
+                pointBordure[i].layer = LayerMask.NameToLayer("Radar");
+                pointeurs[i].layer = LayerMask.NameToLayer("Invisible");
+            }
+            else
+            {
+                pointBordure[i].layer = LayerMask.NameToLayer("Invisible");
+                pointeurs[i].layer = LayerMask.NameToLayer("Radar");
+
+            }
+        }
         
     }
     void creationObjetsRadar()
     {
         pointeurs = new List<GameObject>();
-        foreach(GameObject o in traqueurs)
+        pointBordure = new List<GameObject>();
+        foreach (GameObject o in traqueurs)
         {
             GameObject k = Instantiate(point, o.transform.position, Quaternion.identity) as GameObject;
             pointeurs.Add(k);
+            GameObject j = Instantiate(point, o.transform.position, Quaternion.identity) as GameObject;
+            pointBordure.Add(j);
         }
     }
 }

788796bf3790924ced8332b2b6aaf42682287da0
logandej <74404279+logandej@users.noreply.github.com>
2021-01-19 19:43:52 +0100

    taharus

---
+++ Assets/Scenes/Taharus/bouton1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouton1 : MonoBehaviour
+{
+    public bool Bout1 = false;
+    public MeshRenderer B1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter()
+    {
+        if (Bout1 == false)
+        {
+            Bout1 = true;
+            B1.enabled = false;
+            Debug.Log("b1");
+
+        }
+    }
+}
---
+++ Assets/Scenes/Taharus/bouton3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouton3 : MonoBehaviour
+{
+    public bool Bout3 = false;
+    public MeshRenderer B3;
+    public GameObject PorteDonjonS;
+    public bouton2 B2;
+    public bouton1 B1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Bout3 == true && B2.Bout2 == true && B1.Bout1 == true)
+        {
+            Debug.Log("Une porte s'est ouverte");
+            PorteDonjonS.SetActive(false);
+        }
+    }
+    void OnCollisionEnter()
+    {
+        if (Bout3 == false)
+        {
+            Bout3 = true;
+            B3.enabled = false;
+            Debug.Log("b3");
+
+        }
+    }
+}

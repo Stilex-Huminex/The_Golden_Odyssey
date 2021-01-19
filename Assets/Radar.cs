3818511aee2423e4015d9a4a0caccca934671dd4
1Samper1 <quentin.arzalier@free.fr>
2021-01-19 10:54:51 +0100

    plus plus de radar

--- Assets/Radar.cs
+++ Assets/Radar.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Radar : MonoBehaviour
 {
     [SerializeField] private GameObject[] traqueurs;
     List<GameObject> pointeurs;
+    [SerializeField] private GameObject point;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,10 @@ public class Radar : MonoBehaviour
     }
     void creationObjetsRadar()
     {
-
+        pointeurs = new List<GameObject>();
+        foreach(GameObject o in traqueurs)
+        {
+            GameObject k = Instantiate(point, o.transform.position, Quaternion.identity) as GameObject;
+        }
     }
 }

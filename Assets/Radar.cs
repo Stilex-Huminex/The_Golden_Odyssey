9e21c7e315e5792e545ed4c9bc305d73c9656b8a
1Samper1 <quentin.arzalier@free.fr>
2021-01-19 10:47:25 +0100

    plus de radar

--- Assets/Radar.cs
+++ Assets/Radar.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Radar : MonoBehaviour
 {
     [SerializeField] private GameObject[] traqueurs;
+    List<GameObject> pointeurs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        creationObjetsRadar();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void creationObjetsRadar()
+    {
+
     }
 }

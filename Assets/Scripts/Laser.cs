a36f975ebfed8b3b1fd4ae9110de045f6ca1cfcd
MathTheFolf <61051408+MathTheFolf@users.noreply.github.com>
2021-01-19 15:57:24 +0100

    Pewpew

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject PrefLaz;
+    public GameObject pointFeu;
+
+    private GameObject spawnedLaser;
+    void Start()
+    {
+        spawnedLaser = Instantiate(PrefLaz, pointFeu.transform) as GameObject;
+        DesactiverLaser();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ActiverLaser();
+        }
+        if (Input.GetMouseButton(1)) 
+        {
+            MAJLaser();        
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            DesactiverLaser();
+        }
+    }
+
+    void ActiverLaser()
+    {
+        spawnedLaser.SetActive(true);
+    }
+
+    void DesactiverLaser()
+    {
+        spawnedLaser.SetActive(false);
+    }
+    void MAJLaser()
+    {
+        if(pointFeu!=null)
+        {
+            spawnedLaser.transform.position = pointFeu.transform.position;
+        }
+    }
+
+}

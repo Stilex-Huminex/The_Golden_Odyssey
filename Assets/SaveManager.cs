fbaf36e4205cd0cee56cc269ec495d8d7bff73c7
1Samper1 <quentin.arzalier@free.fr>
2021-01-19 20:46:54 +0100

    prefab joueur changed

---
+++ Assets/SaveManager.cs
@@ -0,0 +1,10 @@
+﻿public static class SaveManager
+{
+    public static bool isGrapUnlocked = false;
+    public static bool isLaserUnlocked = false;
+    public static bool isWaveUnlocked = false;
+
+    public static bool stone1 = false;
+    public static bool stone2 = false;
+    public static bool stone3 = false;
+}

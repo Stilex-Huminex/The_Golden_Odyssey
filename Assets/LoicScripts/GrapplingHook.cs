92ba09670d870e661b066fd74762cb35ad055e64
1Samper1 <quentin.arzalier@free.fr>
2021-01-19 09:12:53 +0100

    Prefab Grappin fini

--- Assets/LoicScripts/GrapplingHook.cs
+++ Assets/LoicScripts/GrapplingHook.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class GrapplingHook : MonoBehaviour
     [SerializeField] private PlayerMovement move;
     private float maxDistance = 100f;
     private SpringJoint joint;
+
+    [SerializeField] private Transform groundCheck; //position sphere
+    [SerializeField] private float groundDistance = 0.4f; // radius of the shpere
+    [SerializeField] private LayerMask groundMask;
 
     void Awake()
     {
@@ -28,6 +33,21 @@ public class GrapplingHook : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             StopGrapple();
+        }
+
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, groundMask))
+        {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+            contr.enabled = true;
+            move.enabled = true;
+        }
+        else if (IsGrappling())
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            contr.enabled = false;
+            move.enabled = false;
         }
     }
 
@@ -53,18 +73,43 @@ public class GrapplingHook : MonoBehaviour
             joint.spring = 4.5f;
             joint.damper = 7f;
             joint.massScale = 4.5f;
+
+            lr.positionCount = 2;
+            currentGrapplePosition = watchTip.position;
         }
     }
 
+    /// <summary>
+    /// Call whenever we want to stop a grapple
+    /// </summary>
     void StopGrapple()
     {
-
+        lr.positionCount = 0;
+        Destroy(joint);
     }
 
-    void DrawRope() 
+
+    private Vector3 currentGrapplePosition;
+
+    void DrawRope()
     {
+        //If not grappling, don't draw rope
+        if (!joint) return;
+
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, pointGrappin, Time.deltaTime * 8f);
+
         lr.SetPosition(0, watchTip.position);
-        lr.SetPosition(1, pointGrappin);
+        lr.SetPosition(1, currentGrapplePosition);
+    }
+
+    public bool IsGrappling()
+    {
+        return joint != null;
+    }
+
+    public Vector3 GetGrapplePoint()
+    {
+        return pointGrappin;
     }
 
 

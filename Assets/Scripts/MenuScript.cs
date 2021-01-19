63df9305262876e2bc389dfa86fe3f968e1f9613
Fabien Zoccola <biothewolff@gmx.fr>
2021-01-19 01:24:07 +0100

    feat(options): added a nice settings menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+
+    public void goToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void goToOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}

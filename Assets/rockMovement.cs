ef6480f7633c825dae44148197d10add8da67018
1Samper1 <quentin.arzalier@free.fr>
2021-01-20 01:26:25 +0100

    ça poushe

--- Assets/rockMovement.cs
+++ Assets/rockMovement.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class rockMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector3 spawn;
     private void Start()
     {
+        spawn = transform.position;
         rb = transform.gameObject.GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        if (transform.position.y <= 0)
+        {
+            transform.position = spawn;
+            rb.velocity = new Vector3(0,0,0)
+        }
+    }
+
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3(0, 0, 5));

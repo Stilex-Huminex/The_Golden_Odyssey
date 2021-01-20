37f3ffbff67580a57d32affacc22ba69482c9ab4
1Samper1 <quentin.arzalier@free.fr>
2021-01-20 07:14:50 +0100

    LEs plaques remarchent

--- Assets/altarCollider.cs
+++ Assets/altarCollider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,23 +40,23 @@ public class altarCollider : MonoBehaviour
     {
         if (collision.collider.name == "First Person Player")
         {
-            switch (id)
+            if (id == 0 && !SaveManager.isGrapUnlocked)
             {
-                case 0:
-                    SaveManager.isGrapUnlocked = true;
-                    rend.material = materials[0];
-                    StartCoroutine(DrawPopup("Grappin débloqué, utilisez la touche 2 pour l'équiper."));
-                    break;
-                case 1:
-                    SaveManager.isLaserUnlocked = true;
-                    rend.material = materials[1];
-                    StartCoroutine(DrawPopup("Laser débloqué, utilisez la touche 3 pour l'équiper."));
-                    break;
-                case 2:
-                    SaveManager.isWaveUnlocked = true;
-                    rend.material = materials[2];
-                    StartCoroutine(DrawPopup("Onde débloquée, utilisez la touche 4 pour l'équiper."));
-                    break;
+                SaveManager.isGrapUnlocked = true;
+                rend.material = materials[0];
+                StartCoroutine(DrawPopup("Grappin débloqué, utilisez la touche 2 pour l'équiper."));
+            }
+            else if (id == 1 && !SaveManager.isLaserUnlocked)
+            {
+                SaveManager.isLaserUnlocked = true;
+                rend.material = materials[1];
+                StartCoroutine(DrawPopup("Laser débloqué, utilisez la touche 3 pour l'équiper."));
+            }
+            else if (id == 2 && !SaveManager.isWaveUnlocked)
+            {
+                SaveManager.isWaveUnlocked = true;
+                rend.material = materials[2];
+                StartCoroutine(DrawPopup("Onde débloquée, utilisez la touche 4 pour l'équiper."));
             }
         }
     }
